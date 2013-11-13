30d7c3f9c24053d4434425afd4eedff50ce826b5
ubuntu <ubuntu@ubuntu-Inspiron-7520.(none)>
2013-11-13 11:14:49 +0200

    1m url dosyadan cekip siralamasi hbase eklendi

---
+++ urlswithhappybase/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: cp1254 -*-
+
+import subprocess
+import os
+import hbase
+import alexa
+
+if __name__ == '__main__':
+    if os.path.exists('test.cron'):
+        try:
+                f = open('test.cron', 'w')
+                cronstmt = "* 12,15 15 * * ./~/1M/main.py\n"
+                satir = f.write(cronstmt)
+                f.close()
+        except:
+                print "dosya editlenemedi..."
+        subprocess.call("crontab test.cron", shell=True)
+        # os.remove("test.cron")
+        if(os.path.exists('top-1m.zip')):
+            try:
+                #'top-1m.zip'
+                eski = os.path.getsize('top-1m.zip')
+                #Dosya.setSize(eski)
+                print 'eski....:  ', eski
+            except:
+                raise
+                print 'dosya silinmis.'
+    hbase = hbase.HBase()
+    object1 = alexa.Alexa(eski)
+    for i in range(len(object1.reversedurls)):
+        hbase.checkRow(object1.reversedurls[i], i + 1)
+
+
+    
